ee58f84bc540f7f90cbabcf3caad0d42450be416
Grant Yoshitsu <grantkoji@gmail.com>
2020-04-06 02:05:59 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -2,9 +2,9 @@ function forLoop() {
   var array;
   for (let i = 0; i < 25; i++) {
     if (i === 0) {
-      array.push("I am 1 strange loop.");
+      var array = [...array, "I am 1 strange loop."];
     } else {
-      array.push(`"I am ${i} strange loops."`);
+      var array = [...array, `"I am ${i} strange loops."`];
   }
   }
   return array
